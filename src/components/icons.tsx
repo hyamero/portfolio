52072d02aaa3e7e274d213d2138e6e098f63cc10
hyamero <daleban.dev@gmail.com>
2024-05-17 16:48:20 +0800

    feat: icons component

---
+++ src/components/icons.tsx
@@ -0,0 +1,20 @@
+type IconProps = React.HTMLAttributes<SVGElement>;
+
+export const Icons = {
+  arrowUpRight: (props: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="h-5 w-5 stroke-[1.5]"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+      />
+    </svg>
+  ),
+};
