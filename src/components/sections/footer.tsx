6c19301d2d17b96b893a279b5cfb11b8c985c48a
hyamero <daleban.dev@gmail.com>
2024-06-10 19:16:56 +0800

    feat: footer design

---
+++ src/components/sections/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="relative mt-20 w-screen rounded-t-lg border-t bg-[#111] py-14">
+      <p className="text-center text-muted-foreground">
+        &copy; 2024 Joseph Dale Bañares ·{" "}
+        <a href="https://github.com/hyamero">hyamero</a>
+      </p>
+    </footer>
+  );
+}
