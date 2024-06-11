fb25d8960656af29880ed252b6995649fa8325f9
hyamero <daleban.dev@gmail.com>
2024-06-11 15:58:06 +0800

    refactor: apply resusable wrapper for subtitle

--- src/lib/projects.tsx
+++ src/lib/projects.tsx
@@ -1,8 +1,8 @@
+import React from "react";
 import { type StaticImageData } from "next/image";
 import omsThumb from "/public/img/projects/omsimos.jpg";
 import umaminThumb from "/public/img/projects/umamin.jpg";
 import foliageThumb from "/public/img/projects/foliage.jpg";
-import React from "react";
 
 export type Project = {
   Subtitle: () => React.JSX.Element;
@@ -14,16 +14,22 @@ export type Project = {
   techs: string;
 };
 
+const SubtitleWrapper = ({ children }: { children: React.ReactNode }) => (
+  <h2 className="pointer-events-auto w-full  2xl:w-4/5 [&>span]:font-medium [&>span]:text-foreground">
+    {children}
+  </h2>
+);
+
 export const projects: Project[] = [
   {
     company: "omsimos",
     description: "is a community driven open source developer collective",
     Subtitle: () => (
-      <h2 className="pointer-events-auto w-full  2xl:w-4/5 [&>span]:font-medium [&>span]:text-foreground">
+      <SubtitleWrapper>
         I specialize in crafting <span>high-quality</span> websites using{" "}
         <span>cutting-edge</span> technologies, seamlessly blending creative
         design with <span>top-tier</span> performance.
-      </h2>
+      </SubtitleWrapper>
     ),
     image: omsThumb,
     year: 2023,
@@ -34,11 +40,11 @@ export const projects: Project[] = [
     company: "umamin",
     description: "is an open-source platform for anonymous messages.",
     Subtitle: () => (
-      <h2 className="pointer-events-auto w-full  2xl:w-4/5 [&>span]:font-medium [&>span]:text-foreground">
+      <SubtitleWrapper>
         Reached over <span>2.5 million unique users</span> and generated{" "}
         <span>14 million+ page visits</span>. Attained over 700K users in less
         than a week during the initial launch.
-      </h2>
+      </SubtitleWrapper>
     ),
     image: umaminThumb,
     year: 2023,
@@ -50,11 +56,11 @@ export const projects: Project[] = [
     description:
       "is an experimental e-commerce website design for luxurious plants.",
     Subtitle: () => (
-      <h2 className="pointer-events-auto w-full  2xl:w-4/5 [&>span]:font-medium [&>span]:text-foreground">
+      <SubtitleWrapper>
         Excels in crafting <span>clean</span> and{" "}
         <span>interactive websites</span>, seamlessly blending minimalist design
         with <span>modern technology</span> for exceptional user experiences.
-      </h2>
+      </SubtitleWrapper>
     ),
     image: foliageThumb,
     year: 2023,
