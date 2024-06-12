f2cae8b20b2f9cd2cc20b683c1dee883650df0aa
hyamero <daleban.dev@gmail.com>
2024-06-12 13:05:29 +0800

    refactor: apply gsap context for animation cleanup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,41 +1,47 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import gsap from "gsap";
+import React, { useEffect, useRef, useState } from "react";
 import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
-import { closeMenu, openMenu, scrollTo } from "@/gsap/navbar-animation";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-const navItems = ["tools", "design", "contact"];
+const menuOpen: GSAPTimeline = gsap.timeline();
+const menuClose: GSAPTimeline = gsap.timeline();
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollToPlugin);
+
 const navProjects = ["omsimos", "umamin", "foliage"];
+const navItems = ["tools", "design", "contact"];
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleMenu = () => {
-    setIsOpen(!isOpen);
+  const { contextSafe } = useGSAP();
 
-    if (!isOpen) {
-      openMenu();
-    } else {
-      closeMenu();
-    }
-  };
+  const scrollTo = contextSafe((scrollElement: string, offsetY: number) => {
+    setIsOpen(false);
+
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: `#${scrollElement}`, offsetY },
+      ease: "power2.easeOut",
+    });
+  });
 
   /**
    * Close nav when clicked outside
    */
-  const divRef = useRef<HTMLDivElement>(null);
+  const navRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
         isOpen &&
-        divRef.current &&
-        !divRef.current.contains(event.target as Node)
+        navRef.current &&
+        !navRef.current.contains(event.target as Node)
       ) {
         setIsOpen(false);
-        closeMenu();
-        console.log("run this");
       }
     };
 
@@ -45,15 +51,83 @@ export default function Navbar() {
     };
   }, [isOpen]);
 
+  useGSAP(
+    () => {
+      if (isOpen) {
+        menuOpen
+          .to(".menu", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease: "power2.easeInOut",
+            duration: 0.5,
+          })
+          .to(
+            "body",
+            {
+              overflowY: "hidden",
+            },
+            "<",
+          )
+          .fromTo(
+            ".menu-item, .menu-item-title",
+            {
+              y: -150,
+              opacity: 0,
+              skewX: -10,
+            },
+            {
+              y: 0,
+              opacity: 1,
+              skewX: 0,
+              duration: 0.4,
+              stagger: 0.1,
+              ease: "power4.out",
+            },
+            "-=0.1",
+          );
+      } else {
+        menuClose
+          .fromTo(
+            ".menu-item, .menu-item-title",
+            {
+              y: 0,
+              opacity: 1,
+            },
+            {
+              opacity: 0,
+              y: -150,
+              duration: 0.4,
+              stagger: 0.1,
+              ease: "power4.out",
+            },
+          )
+          .to(
+            "body",
+            {
+              overflowY: "auto",
+            },
+            "<",
+          )
+          .to(
+            ".menu",
+            {
+              clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+              ease: "power2.easeInOut",
+              duration: 0.5,
+            },
+            "-=0.4",
+          );
+      }
+    },
+    { dependencies: [isOpen], scope: navRef },
+  );
+
   return (
-    <div ref={divRef}>
+    <div ref={navRef}>
       <nav className="container fixed left-0 right-0 top-0 z-50 mx-auto mt-10 flex justify-between text-foreground mix-blend-difference">
         <button
           className="nav-item text-xl font-normal tracking-tighter"
           onClick={() => {
-            closeMenu();
             scrollTo("home", 0);
-            setIsOpen(false);
           }}
         >
           hyamero.
@@ -62,7 +136,7 @@ export default function Navbar() {
         <button
           type="button"
           className="nav-item menu-burger group flex w-8 cursor-pointer flex-col items-center justify-center space-y-1 py-3 [&>span]:block [&>span]:h-[1.5px] [&>span]:w-full [&>span]:transform [&>span]:bg-foreground [&>span]:transition [&>span]:duration-300"
-          onClick={() => handleMenu()}
+          onClick={() => setIsOpen(!isOpen)}
         >
           <span
             className={`${
@@ -83,12 +157,16 @@ export default function Navbar() {
       {/**
        * MENU COMPONENT
        */}
-      <Menu handleMenu={handleMenu} />
+      <Menu scrollTo={scrollTo} />
     </div>
   );
 }
 
-const Menu = ({ handleMenu }: { handleMenu: () => void }) => {
+const Menu = ({
+  scrollTo,
+}: {
+  scrollTo: (scrollElement: string, offsetY: number) => void;
+}) => {
   useEffect(() => {
     const menuItems = Array.from(document.querySelectorAll(".menu-item"));
     menuItems.forEach((item: any) => {
@@ -106,25 +184,13 @@ const Menu = ({ handleMenu }: { handleMenu: () => void }) => {
     });
   }, []);
 
-  gsap.registerPlugin(ScrollToPlugin);
-
-  const scrollToSection = (scrollElement: string, offsetY: number) => {
-    handleMenu();
-
-    gsap.to(window, {
-      duration: 1,
-      scrollTo: { y: `#${scrollElement}`, offsetY },
-      ease: "power2.easeOut",
-    });
-  };
-
   return (
     <div className="menu fixed left-0 top-0 z-30 grid h-screen w-screen place-items-center justify-center space-y-1 rounded-lg border-b bg-[#111] bg-opacity-35 text-secondary backdrop-blur-lg [clipPath:polygon(0%_0%,_100%_0%,_100%_0%,_0%_0%)] sm:h-[65vh]">
       <div className="flex flex-col gap-14 sm:flex-row sm:gap-28">
         <div className="flex flex-col items-start gap-3 md:gap-5">
           <button
             onClick={() => {
-              scrollToSection("home", 0);
+              scrollTo("home", 0);
             }}
             type="button"
             className="menu-item-title cursor-pointer text-left text-xs font-medium uppercase text-muted-foreground opacity-0"
@@ -137,7 +203,7 @@ const Menu = ({ handleMenu }: { handleMenu: () => void }) => {
               key={item}
               className="menu-item text-4xl font-light tracking-tighter opacity-0 md:text-5xl"
               onClick={() => {
-                scrollToSection(item, 0);
+                scrollTo(item, 0);
               }}
             >
               <div className="col-start-2 row-start-2 flex font-light tracking-tighter">
@@ -150,7 +216,7 @@ const Menu = ({ handleMenu }: { handleMenu: () => void }) => {
         <div className="flex flex-col items-start gap-3 md:gap-5">
           <button
             onClick={() => {
-              scrollToSection("omsimos", 0);
+              scrollTo("omsimos", 0);
             }}
             type="button"
             className="menu-item-title cursor-pointer text-left text-xs font-medium uppercase text-muted-foreground opacity-0"
@@ -163,7 +229,7 @@ const Menu = ({ handleMenu }: { handleMenu: () => void }) => {
               key={item}
               className="menu-item text-4xl font-light tracking-tighter opacity-0 md:text-5xl"
               onClick={() => {
-                scrollToSection(item, 0);
+                scrollTo(item, 0);
               }}
             >
               <div className="col-start-2 row-start-2 flex font-light tracking-tighter">
