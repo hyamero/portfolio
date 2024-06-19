5c1aa6dbc8c7e97f3590af74159f4236fdcc38b1
hyamero <daleban.dev@gmail.com>
2024-06-19 19:27:13 +0800

    chore: set default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={GeistSans.className}>
         <Navbar />
         <CommandMenu />
