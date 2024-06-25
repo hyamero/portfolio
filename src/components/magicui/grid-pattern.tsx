3fdaa2129148c025af3a4ac1b68f417785e229ba
hyamero <daleban.dev@gmail.com>
2024-06-25 23:47:01 +0800

    chore: random filled grid square animation

--- src/components/magicui/grid-pattern.tsx
+++ src/components/magicui/grid-pattern.tsx
@@ -2,7 +2,7 @@
 
 import gsap from "gsap";
 import { cn } from "@/lib/utils";
-import { useId } from "react";
+import { useId, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP);
@@ -30,6 +30,68 @@ export function GridPattern({
 }: GridPatternProps) {
   const id = useId();
 
+  const tl = useRef<GSAPTimeline>();
+
+  useGSAP(() => {
+    tl.current = gsap
+      .timeline({ repeat: -1 })
+      .to(".rect-item", {
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.5,
+      })
+      .to(".rect-item", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.5,
+      });
+  });
+
+  function calculateDistance(
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+  ): number {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  }
+
+  function getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function generateRandomSquares(
+    count: number,
+    max: number,
+    minDistance: number,
+  ): [x: number, y: number][] {
+    const squares: [x: number, y: number][] = [];
+    const generatedPairs: Set<string> = new Set(); // Set to store generated pairs as strings "x,y"
+
+    while (squares.length < count) {
+      const x = getRandomInt(10, max);
+      const y = getRandomInt(10, max);
+      let isValid = true;
+
+      // Check distance from existing squares
+      for (const [px, py] of squares) {
+        if (calculateDistance(x, y, px, py) < minDistance) {
+          isValid = false;
+          break;
+        }
+      }
+
+      if (isValid) {
+        const pairString = `${x},${y}`;
+        squares.push([x, y]);
+        generatedPairs.add(pairString);
+      }
+    }
+
+    return squares;
+  }
+
+  const randomSquares = generateRandomSquares(15, 30, 5);
+
   return (
     <svg
       aria-hidden="true"
@@ -56,9 +118,9 @@ export function GridPattern({
         </pattern>
       </defs>
       <rect width="100%" height="100%" strokeWidth={0} fill={`url(#${id})`} />
-      {squares && (
+      {randomSquares && (
         <svg x={x} y={y} className="overflow-visible">
-          {squares.map(([x, y]) => (
+          {randomSquares.map(([x, y]) => (
             <rect
               className="rect-item opacity-0"
               strokeWidth="0"
