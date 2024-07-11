7f797ed77f2bbe1354bd1b1ce91c98266a934266
Dale B <daleban.dev@gmail.com>
2024-07-11 15:45:31 +0800

    refactor: remove unused backdrop filters

--- src/components/sections/project-showcase.tsx
+++ src/components/sections/project-showcase.tsx
@@ -25,14 +25,14 @@ export default function ProjectShowcase({ ...props }: Project) {
       className="border-b-none pointer-events-none  relative z-10 pb-14 lg:border-b lg:pb-0"
     >
       <div className="container grid min-h-screen grid-cols-4 items-center pt-40 lg:gap-y-0">
-        <div className="col-span-4 col-start-1 row-start-1 h-full w-full text-pretty rounded-lg text-[clamp(1.3rem,3.3vw,3rem)] leading-[1.3] tracking-tighter text-muted-foreground backdrop-blur-sm sm:px-10 sm:py-10 md:leading-[1.2] lg:border lg:py-24 lg:leading-[1.1] xl:text-balance xl:rounded-br-none">
+        <div className="col-span-4 col-start-1 row-start-1 h-full w-full text-pretty rounded-lg text-[clamp(1.3rem,3.3vw,3rem)] leading-[1.3] tracking-tighter text-muted-foreground sm:px-10 sm:py-10 md:leading-[1.2] lg:border lg:py-24 lg:leading-[1.1] xl:text-balance xl:rounded-br-none">
           <Subtitle />
         </div>
 
         <div className="col-span-4 col-start-1 row-start-2 my-16 sm:px-12 lg:col-span-3 lg:my-0 lg:p-12 2xl:p-24">
           <ShineBorder
             color={["#1e1e1e", shineColor[1]]}
-            className="overflow-hidden backdrop-blur-sm"
+            className="overflow-hidden"
           >
             <div className="rounded-lg border bg-[#111] bg-opacity-20 p-2 md:p-4">
               <Image
@@ -46,7 +46,7 @@ export default function ProjectShowcase({ ...props }: Project) {
           </ShineBorder>
         </div>
 
-        <div className="col-span-4 row-start-4 flex flex-col items-center justify-center gap-14 rounded-b-lg border-x border-b px-10 py-10 backdrop-blur-sm sm:flex-row md:rounded-none lg:col-start-4 lg:row-start-2 lg:h-full lg:flex-col lg:border-t-0 lg:px-7 lg:py-0 xl:rounded-br-lg">
+        <div className="col-span-4 row-start-4 flex flex-col items-center justify-center gap-14 rounded-b-lg border-x border-b px-10 py-10 sm:flex-row md:rounded-none lg:col-start-4 lg:row-start-2 lg:h-full lg:flex-col lg:border-t-0 lg:px-7 lg:py-0 xl:rounded-br-lg">
           <Image
             alt="Atom Icon"
             src={atomIcon}
@@ -82,7 +82,7 @@ export default function ProjectShowcase({ ...props }: Project) {
                 </a>
               </Button>
             </div>
-            <div className="hidden h-full border-t backdrop-blur-[1.5px] lg:block" />
+            <div className="hidden h-full border-t lg:block" />
           </div>
 
           <div className="col-span-4 bg-[#111] md:col-span-3 md:col-start-3 lg:col-span-2">
@@ -100,7 +100,7 @@ export default function ProjectShowcase({ ...props }: Project) {
                 <span className="text-right">{role}</span>
               </li>
             </ul>
-            <div className="hidden h-full border-l border-t backdrop-blur-[1.5px] lg:block" />
+            <div className="hidden h-full border-l border-t lg:block" />
           </div>
         </div>
       </div>
--- src/components/transition-loader.tsx
+++ src/components/transition-loader.tsx
@@ -7,7 +7,7 @@ gsap.registerPlugin(useGSAP);
 
 export default function TransitionLoader() {
   return (
-    <div className="banner [&>*]:fixed [&>*]:top-0 [&>*]:z-50 [&>*]:min-h-screen [&>*]:w-1/4 [&>*]:bg-[#111]">
+    <div className="banner [&>*]:fixed [&>*]:top-0 [&>*]:z-50 [&>*]:min-h-screen [&>*]:w-1/4 [&>*]:border-r [&>*]:bg-[#111]">
       <div className="left-0" />
       <div className="left-1/4" />
       <div className="left-2/4" />
