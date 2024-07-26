767d0c69bb59a3868d85527c31613c9f8e46d547
Dale B <daleban.dev@gmail.com>
2024-07-26 23:41:17 +0800

    chore: simple footer design

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,10 +1,39 @@
+"use client";
+
+import gsap from "gsap";
+import { ArrowUpIcon } from "lucide-react";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+
 export default function Footer() {
+  gsap.registerPlugin(ScrollToPlugin);
+
+  const scrollToSection = (scrollElement: string, offsetY: number) => {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: `#${scrollElement}`, offsetY },
+      ease: "power2.easeOut",
+    });
+  };
+
   return (
-    <footer className="relative mt-40 w-full rounded-t-lg border-t bg-neutral-950 py-10 md:py-14">
-      <p className="text-center text-xs text-muted-foreground md:text-base">
-        &copy; 2024 Joseph Dale Bañares ·{" "}
-        <a href="https://github.com/hyamero">hyamero</a>
-      </p>
+    <footer
+      id="footer"
+      className="relative mt-52 border-t bg-neutral-950 py-16 pb-12 lg:mt-64 lg:py-28"
+    >
+      <div className="container flex flex-col items-center gap-5 lg:flex-row lg:items-end lg:justify-between">
+        <p className="text-[clamp(2rem,16vw,12rem)] font-semibold leading-none tracking-tighter text-neutral-200">
+          hyamero
+        </p>
+
+        <div className="mb-5 flex items-center gap-2 text-neutral-400 lg:gap-5">
+          <p className="text-base font-light tracking-tight sm:text-lg lg:text-xl">
+            Copyright &copy; Dale Bañares 2024
+          </p>
+          <button onClick={() => scrollToSection("home", 0)}>
+            <ArrowUpIcon />
+          </button>
+        </div>
+      </div>
     </footer>
   );
 }
